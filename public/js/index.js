a7b167293ae510cf095ebb4a37ab327c9897a6ef
asrulik <asulik@gmail.com>
2017-12-11 16:32:05 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Srulik',
-        text: 'Hey, this is Srulik.'
-    });
 });
 
 socket.on('disconnect', function() {
